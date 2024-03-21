774d4c316951199cd47fc3f74416409c8b6e9e17
rir7890 <Rahul Kumar>
2024-03-21 23:36:50 +0530

    flag of the country to indentify

---
+++ index.js
@@ -0,0 +1,76 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { Client } from "pg";
+
+const app = express();
+const port = 3000;
+
+let quiz = [];
+const client = new Client({
+  username: "postgres",
+  password: "12345",
+  host: "localhost",
+  port: 5432,
+  database: "world",
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("postgre sql database is connected");
+  })
+  .catch((err) => {
+    console.log("error is connecting the postgre sql");
+  });
+
+client.query("SELECT *FROM flags ", (err, results) => {
+  if (err) {
+    console.error("Error in executing query ", err);
+  } else {
+    quiz = results.rows;
+  }
+  client.end();
+});
+
+let totalCorrect = 0;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentQuestion = {};
+
+// GET home page
+app.get("/", (req, res) => {
+  totalCorrect = 0;
+  nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
